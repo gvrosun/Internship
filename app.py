5ba087a24806d1e445f495f4b96d8445911632e3
Rosun GV <gvrosun@gmail.com>
2021-05-07 21:29:13 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+import os.path
+
+from flask import Flask, render_template, redirect, url_for
+from forms import StoredForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+base_dir = os.path.dirname(__file__)
+
+app.config['SECRET_KEY'] = 'my_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+
+# Models
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    message = db.Column(db.Text)
+
+    def __init__(self, name, message):
+        self.name = name
+        self.message = message
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/reset')
+def reset():
+    return redirect(url_for('index'))
+
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+    form = StoredForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        message = form.message.data
+        new_data = Comments(name, message)
+        db.session.add(new_data)
+        db.session.commit()
+        return redirect(url_for('blog'))
+
+    return render_template('blog.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
